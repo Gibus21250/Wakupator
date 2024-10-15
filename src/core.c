8502437fc2c50ee3f0af3af50132081058aea3d9
Nathan J. <jacquinet.nathan@gmail.com>
2024-10-15 20:42:11 +0200

    Update error messages

--- src/core.c
+++ src/core.c
@@ -219,24 +219,25 @@ const char* get_register_error(REGISTER_CODE code)
         case OK: return "OK.";
         case OUT_OF_MEMORY: return "Out of memory on the host.";
 
-        case PARSING_CJSON_ERROR: return "An error was find in the JSON. Please verify types, keynames and structure.";
+        case PARSING_CJSON_ERROR: return "An error has been found in the JSON. Please check the types, key names and structure.";
         case PARSING_INVALID_MAC_ADDRESS: return "Invalid MAC address format.";
-        case PARSING_DUPLICATED_IP_ADDRESS: return "Duplicated IP asked.";
         case PARSING_INVALID_IP_ADDRESS: return "Invalid IP address format.";
+        case PARSING_DUPLICATED_IP_ADDRESS: return "A duplicate IP has been found in the JSON, please merge all ports in an array for this IP.";
         case PARSING_INVALID_PORT: return "Invalid port value.";
 
-        case MANAGER_MAC_ADDRESS_ALREADY_MONITORED: return "A client with this MAC address is already monitored.";
-        case MANAGER_THREAD_CREATION_ERROR: return "Intern error while creating thread monitor.";
-        case MANAGER_THREAD_INIT_ERROR: return "Error while init information for the thread monitor.";
-        case MANAGER_THREAD_INIT_TIMEOUT: return "Thread's initializing state took too much time.";
+        case MANAGER_MAC_ADDRESS_ALREADY_MONITORED: return "A client with this MAC address is already being monitored.";
+        case MANAGER_THREAD_CREATION_ERROR: return "Internal error when creating the monitor thread.";
+        case MANAGER_THREAD_INIT_ERROR: return "Error during initialisation of information for the monitor thread.";
+        case MANAGER_THREAD_INIT_TIMEOUT: return "The initialization state of the monitor thread has taken too long.";
 
-        case MONITOR_DAD_ERROR: return "Impossible to temporally disable IPv6 Duplicate Address Detector.";
-        case MONITOR_CHECK_IP_ERROR: return "Error while executing command to check IP duplication.";
-        case MONITOR_RAW_SOCKET_CREATION_ERROR: return "Error while creating raw socket for the client.";
-        case MONITOR_IP_ALREADY_USED: return "A client have already register one of IPs asked.";
+        case MONITOR_DAD_ERROR: return "Unable to temporarily disable the IPv6 duplicate address detector.";
+        case MONITOR_CHECK_IP_ERROR: return "Error when executing the IP duplication verification command.";
+        case MONITOR_RAW_SOCKET_CREATION_ERROR: return "Error when creating a raw socket for the client.";
+        case MONITOR_IP_ALREADY_USED: return "A client has already registered one of the requested IP addresses.";
     }
     return "";
 }
+
 char *get_client_str_info(const client *cl)
 {
     size_t size = 0;
